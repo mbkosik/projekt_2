03418687e6e84df447231842a9989a2b5a2b35e3
mbkosik <72941144+mbkosik@users.noreply.github.com>
2021-04-10 16:27:58 +0200

    click on submenu elements on mobile view

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 
 const toggleBars = document.querySelector("div.toggle");
 const mainNav = document.querySelector("ul.nav-list");
+const dropdownElement = document.querySelectorAll(".dropdown-element")
+const navSubList = document.querySelectorAll("ul.nav-sublist")
 
 toggleBars.addEventListener("click", () => {
     mainNav.classList.toggle("active"); 
 })
+
+if(window.innerWidth <= 500) {
+    dropdownElement[0].addEventListener("click", () => {            navSubList[0].classList.toggle("active"); 
+    console.log(navSubList[0]);
+    })
+
+    dropdownElement[1].addEventListener("click", () => {
+    navSubList[1].classList.toggle("active"); 
+    console.log(navSubList[1]);
+    })
+}
+
+
+
+
